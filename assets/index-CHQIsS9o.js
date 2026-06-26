7477bdf7a2cde9ff0ad72255f5f9a32fff2aaf04
AmirabbasBagheri <amirabaasbagheri@gmail.com>
2026-06-26 17:33:44 +0330

    Updates

--- assets/index-CHQIsS9o.js
+++
@@ -1,5 +0,0 @@
-(function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin===`use-credentials`?t.credentials=`include`:e.crossOrigin===`anonymous`?t.credentials=`omit`:t.credentials=`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();var e=document.querySelector(`.adsBanner`),t=``;async function n(n){try{t=(await(await fetch(`http://localhost:3000/adsBanner`)).json()).map(e=>`
-            <a src="${e.link}">
-            <img src="${e.img}" alt="" class="w-full h-full object-cover">
-            </a>
-        `).join(``),e.innerHTML=t}catch(e){console.log(e)}}n();
